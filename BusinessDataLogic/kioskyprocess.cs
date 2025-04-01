7efdb5b93ff1bdad882b3b025accd4f3d8f65d24
Ceasar789 <philiphermosa087@gmail.com>
2025-04-01 10:42:58 +0800

    commit

---
+++ BusinessDataLogic/kioskyprocess.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessDataLogic
+{
+    public class kiosdata
+    {
+        public static string ProcessOrder(string usrchoice, List<string> currentOrder)
+        {
+            switch (usrchoice)
+            {
+                case "1":
+                    currentOrder.Add("Cheese Burgir");
+                    return "Cheese Burgir Added!";
+                    break;
+                case "2":
+                    currentOrder.Add("Fries");
+                    return "Fries Added!";
+                    break;
+                case "3":
+                    currentOrder.Add("Sundae");
+                    return "Sundae Added!";
+                    break;
+                case "4":
+                    currentOrder.Add("JollyPares");
+                    return "JollyPares Added!";
+                    break;
+                default:
+                    return "Invalid Input. Please press [1-4] to order.";
+                    break;
+            }
+        }
+
+        public static string RemoveItemFromOrder(List<string> currentOrder)
+        {
+            if (currentOrder.Count == 0)
+            {
+                return "Your order cart is empty.";
+            }
+
+            return "Choose your order by pressing numbers [1-4], or press [0] to exit.\nPress [5] to remove item from your order cart.";
+            for (int i = 0; i < currentOrder.Count; i++)
+            {
+                return $"{i + 1}. {currentOrder[i]}";
+            }
+
+            return "Press [0] to cancel.";
+            string removeChoice = Console.ReadLine();
+
+            if (removeChoice == "0")
+            {
+                return "Cancelled.";
+            }
+
+            int itemIndex;
+            if (int.TryParse(removeChoice, out itemIndex) && itemIndex >= 1 && itemIndex <= currentOrder.Count)
+            {
+                string removedItem = currentOrder[itemIndex - 1];
+                currentOrder.RemoveAt(itemIndex - 1);
+                return "Item removed.";
+            }
+            else
+            {
+                return "Invalid Input. Please choose [1-4] only.";
+            }
+        }
+
+        public static string GenerateReceipt(List<string> currentOrder, string serviceChoice)
+        {
+            int totalAmount = 0;
+            string receipt = " Mcdollibee Receipt ";
+            receipt += $"Service: {serviceChoice}";
+            receipt += "Ordered Items:\n";
+
+            foreach (string item in currentOrder)
+            {
+                receipt += $"{item}\n";
+                totalAmount += GetItemPrice(item);
+            }
+            receipt += $"Total: P{totalAmount}\n";
+            receipt += "Thank you for your order! \n";
+            receipt += "Mcdollibee Binan.\n";
+            return receipt;
+        }
+        public static int GetItemPrice(string item)
+        {
+            switch (item)
+            {
+                case "Cheese Burgir":
+                    return 50;
+                case "Fries":
+                    return 40;
+                case "Sundae":
+                    return 35;
+                case "JollyPares":
+                    return 100;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
--- KIOSKY/Program.cs
+++ KIOSKY/Program.cs
@@ -1,142 +1,101 @@
 ﻿using System;
-using System.Collections.Generic;
+using BusinessDataLogic;
 
-namespace kios101
+namespace fixedwmethds
 {
     class Program
     {
         static void Main(string[] args)
         {
-            OrderService orderService = new OrderService();
+            string[] menu = { "[1] [P50 Cheese Burgir]", "[2] [P40 Fries]", "[3] [P35 Sundae]", "[4] [P100 JollyPares]" };
             List<string> receiptHistory = new List<string>();
-
             ShowWelcomeMessage();
+            int start = GetUserChoice();
 
-            int start;
-            do
+            while (start == 1)
             {
-                start = GetUserChoice();
-                if (start == 1)
+                List<string> currentOrder = new List<string>();
+                ShowMenu(menu);
+
+                string usrchoice;
+                while ((usrchoice = GetUserChoiceForOrder()) != "0")
                 {
-                    List<string> currentOrder = new List<string>();
-                    ShowMenu(orderService.GetMenu());
-
-                    string usrchoice;
-                    while ((usrchoice = GetUserChoiceForOrder()) != "0")
+                    if (usrchoice == "5")
                     {
-                        string itemAdded = orderService.ProcessOrder(usrchoice);
-                        if (!string.IsNullOrEmpty(itemAdded))
-                        {
-                            currentOrder.Add(itemAdded);
-                            Console.WriteLine($"{itemAdded} Added!");
-                        }
+                        kiosdata.RemoveItemFromOrder(currentOrder);
                     }
-
-                    string serviceChoice = GetServiceChoice();
-                    string receipt = orderService.GenerateReceipt(currentOrder, serviceChoice);
-                    receiptHistory.Add(receipt);
-
-                    Console.WriteLine(receipt);
-                    Console.WriteLine("Would you like to make another order? (Press [1] for Yes, [0] for No)");
-
-                    if (!int.TryParse(Console.ReadLine(), out start) || (start != 0 && start != 1))
+                    else
                     {
-                        start = 0;
+                        kiosdata.ProcessOrder(usrchoice, currentOrder);
                     }
                 }
-            } while (start == 1);
 
-            Console.WriteLine("Thank you for coming! See you again!");
+                string serviceChoice = GetServiceChoice();
+                string receipt = kiosdata.GenerateReceipt(currentOrder, serviceChoice);
+                receiptHistory.Add(receipt);
+                Console.WriteLine(receipt);
+
+                Console.WriteLine("Would you like to make another order? (Press [1] for Yes, [0] for No)");
+                start = Convert.ToInt32(Console.ReadLine());
+
+                if (start == 0)
+                {
+                    Console.WriteLine("Thank you for coming! See you again!");
+                    break;
+                }
+            }
         }
 
         static void ShowWelcomeMessage()
         {
             Console.WriteLine("Welcome to Mcdollibee Binan! Please press [1] to Order.");
         }
-
         static int GetUserChoice()
         {
             int choice;
             do
             {
-                Console.WriteLine("Press [1] to start ordering.");
-                if (!int.TryParse(Console.ReadLine(), out choice) || choice != 1)
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice != 1)
                 {
                     Console.WriteLine("Invalid Input. Please press [1] to start ordering.");
                 }
             } while (choice != 1);
             return choice;
         }
-
-        static void ShowMenu(List<Menu> menu)
+        static void ShowMenu(string[] menu)
         {
-            Console.WriteLine("\nMcdollibee MENU:");
-            foreach (var item in menu)
+            Console.WriteLine("Mcdollibee MENU:");
+            foreach (string item in menu)
             {
-                Console.WriteLine($"[{item.Id}] {item.Name} - P{item.Price}");
+                Console.WriteLine(item);
             }
         }
-
         static string GetUserChoiceForOrder()
         {
-            Console.WriteLine("Choose your order by pressing numbers [1-4], or press [0] to finish your order.");
+            Console.WriteLine("Choose your order by pressing numbers [1-4], or press [0] to exit.");
+            Console.WriteLine("Press [5] to remove item from your order cart.");
             return Console.ReadLine();
         }
+
+
+
 
         static string GetServiceChoice()
         {
             string serviceChoice;
             do
             {
-                Console.WriteLine("Please choose your service type: [1] Dine-in or [2] Takeout");
+                Console.WriteLine("Please service type : [1] Dine-in or [2] Take-out");
                 serviceChoice = Console.ReadLine();
-
                 if (serviceChoice != "1" && serviceChoice != "2")
                 {
-                    Console.WriteLine("Invalid input! Please select [1] for Dine-in or [2] for Takeout.");
+                    Console.WriteLine("Invalid input! Please select [1] for Dine-in or [2] for Take-out.");
                 }
-
             } while (serviceChoice != "1" && serviceChoice != "2");
 
             return serviceChoice == "1" ? "Dine-in" : "Takeout";
         }
-    }
 
-    class Menu
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Price { get; set; }
-    }
-
-    class OrderService
-    {
-        private List<Menu> menu = new List<Menu>
-        {
-            new Menu { Id = 1, Name = " Cheese Burgir", Price = 45.0 },
-            new Menu { Id = 2, Name = "Fries", Price = 35.0 },
-            new Menu { Id = 3, Name = "Sundae", Price = 30.0 },
-            new Menu { Id = 4, Name = "JollyPares", Price = 100.0 }
-        };
-
-        public List<Menu> GetMenu()
-        {
-            return menu;
-        }
-
-        public string ProcessOrder(string choice)
-        {
-            if (int.TryParse(choice, out int id))
-            {
-                Menu item = menu.Find(m => m.Id == id);
-                return item != null ? item.Name : "";
-            }
-            return "";
-        }
-
-        public string GenerateReceipt(List<string> order, string serviceType)
-        {
-            return $"Receipt:\nItems: {string.Join(", ", order)}\nService: {serviceType}\nTotal: P{order.Count * 50}";
-        }
     }
 }
